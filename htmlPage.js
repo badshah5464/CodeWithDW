791ccd8f8cd23397a4c06fb9975359a79fc380fb
Dev Dave <115370760+badshah5464@users.noreply.github.com>
2023-03-13 20:46:20 +0530

    PageSmartLink

---
+++ htmlPage.js
@@ -0,0 +1,27 @@
+let pageLink = document.getElementById("pageLink");
+
+const page = [
+    {
+        link: "./htmlPages/page01.html",
+        pageName: "page 01",
+    },
+    {
+        link: "./htmlPages/page02.html",
+        pageName: "page 02",
+    },
+    {
+        link: "./htmlPages/page03.html",
+        pageName: "page 03",
+    },
+    {
+        link: "./htmlPages/page04.html",
+        pageName: "page 04",
+    },
+];
+page.map(function (x) {
+    pageLink.innerHTML += `
+        <ul>
+            <li><a href="${x.link}">${x.pageName}</a></li>
+        </ul>
+    `
+})
